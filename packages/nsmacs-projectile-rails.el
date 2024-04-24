7184ac263fd2ff7431bf369bdedfaeb0dc3560cf
Yuta Fujita <ofnhwx@komunan.net>
2024-04-24 16:17:34 +0900

    `projectile', `projectile-rails' の独自拡張を移行

---
+++ packages/nsmacs-projectile-rails.el
@@ -0,0 +1,40 @@
+;;; nsmacs-projectile-rails.el --- projectile-rails 関連のコマンド
+;;; -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'projectile-rails)
+
+;;;###autoload
+(defun e:setup-projectile-rails-views-re ()
+  "Rails のビューファイルとして認識される拡張子を設定する."
+  (let ((exts '("html" "erb" "haml" "slim"
+                "js" "coffee" "ts"
+                "css" "scss" "sass" "less"
+                "json" "builder" "jbuilder" "rabl"
+                "csb" "axlsx")))
+    (setopt projectile-rails-views-re (concat "\\." (regexp-opt exts)))))
+
+;;;###autoload
+(defun e:projectile-rails-find-view-components ()
+  "Find a View component."
+  (interactive)
+  (projectile-rails-find-resource
+   "components: "
+   `(("app/components/" "\\(.+\\)\\.rb$")
+     ("app/components/" ,(concat "\\(.+\\)" projectile-rails-views-re)))
+   "app/components/${filename}"))
+
+;;;###autoload
+(defun e:projectile-rails-find-graphql ()
+  "Find a GraphQL."
+  (interactive)
+  (projectile-rails-find-resource
+   "graphql: "
+   '(("app/graphql/" "\\(.+\\)\\.rb$"))
+   "app/graphql/${filename}"))
+
+(provide 'nsmacs-projectile-rails)
+;;; nsmacs-projectile-rails.el ends here
---
+++ packages/nsmacs-projectile.el
@@ -0,0 +1,24 @@
+;;; nsmacs-projectile.el --- projectile 関連のコマンド
+;;; -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'komunan-lisp-library)
+(require 'projectile)
+
+;;;###autoload
+(defun e:setup-projectile-known-projects ()
+  "`ghq' で管理しているリポジトリを `projectile' で選択できるように設定する."
+  (when (executable-find "ghq")
+    (setq projectile-known-projects
+          (->> projectile-known-projects
+               (--remove (eq (projectile-project-vcs it) 'none))
+               (-union (-map 'f-short (kllib:shell-command-to-list "ghq list --full-path")))
+               (-map 'file-name-as-directory)
+               (-sort 's-less?)
+               (-distinct)))))
+
+(provide 'nsmacs-projectile)
+;;; nsmacs-projectile.el ends here

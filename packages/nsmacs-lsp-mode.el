f439618fcadabd3c9d90d383406e8a7fd41e991f
Yuta Fujita <ofnhwx@komunan.net>
2024-06-06 13:38:27 +0900

    feat: `emacs-lsp-booster` の設定

--- packages/nsmacs-lsp-mode.el
+++ packages/nsmacs-lsp-mode.el
@@ -19,6 +19,32 @@
   (setq-local lsp-solargraph-use-bundler (e:bundle-exists "solargraph")))
 
 ;;;###autoload
+(defun ad:lsp-resolve-final-command@with-lsp-booster (fn command &optional test)
+  "`emacs-lsp-booster' を使って `lsp-server' を起動する.
+FN, COMMAND, TEST はアドバイス対象の関数とその引数."
+  (let ((result (funcall fn command test)))
+    (if (and (not test)
+             (not (file-remote-p default-directory))
+             (bound-and-true-p lsp-use-plists)
+             (not (functionp 'json-rpc-connection))
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (message "Using emacs-lsp-booster for %s!" result)
+          (append '("emacs-lsp-booster" "--json-false-value" ":json-false" "--")
+                  result))
+      result)))
+
+;;;###autoload
+(defun ad:json-read@with-lsp-booster (fn &rest args)
+  "`emacs-lsp-booster' が返すバイトコードを JSON の代わりに読み込む処理.
+FN, ARGS はアドバイス対象の関数とその引数."
+  (or (when (equal (following-char) ?#)
+        (let ((bytecode (read (current-buffer))))
+          (when (byte-code-function-p bytecode)
+            (funcall bytecode))))
+      (apply fn args)))
+
+;;;###autoload
 (defun e:set-lsp-client-add-on (client value)
   "対象の CLIENT の `add-on' の設定を VALUE に変更する."
   (let ((ls (gethash client lsp-clients)))
